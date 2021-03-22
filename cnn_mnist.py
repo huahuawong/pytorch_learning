792d9febd1bf9d16a3d0ccb55b62d6764d5f58b5
JunHua <junhuawong96@gmail.com>
2021-03-21 22:41:57 -0400

    lstm for ecg classifiaction

--- cnn_mnist.py
+++ cnn_mnist.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 import torchvision
 import torchvision.transforms as transforms
 import torchvision.datasets as datasets
+import matplotlib.pyplot as plt
 
 # Check Torch version, currently using 1.6.0 in this project file
 print(torch.__version__)
@@ -38,6 +39,21 @@ test_loader = torch.utils.data.DataLoader(
                              ])),
   batch_size=batch_size_test, shuffle=True)
 
+examples = enumerate(test_loader)
+batch_idx, (example_data, example_targets) = next(examples)
+
+example_data.shape
+
+# Plot out example dataset
+fig = plt.figure()
+for i in range(6):
+  plt.subplot(2,3,i+1)
+  plt.tight_layout()
+  plt.imshow(example_data[i][0], cmap='gray', interpolation='none')
+  plt.title("Ground Truth: {}".format(example_targets[i]))
+  plt.xticks([])
+  plt.yticks([])
+plt.show()
 
 # Convolutional neural network (two convolutional layers)
 class ConvNet(nn.Module):
@@ -46,11 +62,13 @@ class ConvNet(nn.Module):
         self.layer1 = nn.Sequential(
             nn.Conv2d(1, 16, kernel_size=5, stride=1, padding=2),
             nn.BatchNorm2d(16),
+            nn.Dropout2d(0.2),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2))
         self.layer2 = nn.Sequential(
             nn.Conv2d(16, 32, kernel_size=5, stride=1, padding=2),
             nn.BatchNorm2d(32),
+            nn.Dropout2d(0.2),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2))
         self.fc = nn.Linear(7 * 7 * 32, num_classes)
@@ -68,6 +86,11 @@ model = ConvNet(num_classes).to(device)
 # Loss and optimizer, learning rate could be varied
 criterion = nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+train_losses = []
+train_counter = []
+test_losses = []
+test_counter = [i*len(train_loader.dataset) for i in range(num_epochs + 1)]
 
 # Train the model
 total_step = len(train_loader)
@@ -88,12 +111,17 @@ for epoch in range(num_epochs):
         if (i + 1) % 100 == 0:
             print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'
                   .format(epoch + 1, num_epochs, i + 1, total_step, loss.item()))
+            # train_losses.append(loss.item())
+            # train_counter.append(
+            #     (i * 64) + ((epoch) * len(train_loader.dataset)))
+
 
 # Test the model
 model.eval()  # eval mode (batchnorm uses moving mean/variance instead of mini-batch mean/variance)
 with torch.no_grad():
     correct = 0
     total = 0
+    test_loss = 0
     for images, labels in test_loader:
         images = images.to(device)
         labels = labels.to(device)
@@ -101,8 +129,34 @@ with torch.no_grad():
         _, predicted = torch.max(outputs.data, 1)
         total += labels.size(0)
         correct += (predicted == labels).sum().item()
+    #     test_loss += criterion(outputs, labels).item()
+    # test_loss /= len(test_loader.dataset)
+    # test_losses.append(test_loss)
 
     print('Test Accuracy of the model on the 10000 test images: {} %'.format(100 * correct / total))
+
+# Evaluating model's performance, plotting the training curve
+# fig = plt.figure()
+# plt.plot(train_counter, train_losses, color='blue')
+# plt.scatter(test_counter, test_losses, color='red')
+# plt.legend(['Train Loss', 'Test Loss'], loc='upper right')
+# plt.xlabel('number of training examples seen')
+# plt.ylabel('Cross Entropy loss')
+# plt.show()
+
+with torch.no_grad():
+  output = model(example_data)
+
+fig = plt.figure()
+for i in range(6):
+  plt.subplot(2,3,i+1)
+  plt.tight_layout()
+  plt.imshow(example_data[i][0], cmap='gray', interpolation='none')
+  plt.title("Prediction: {}".format(
+    output.data.max(1, keepdim=True)[1][i].item()))
+  plt.xticks([])
+  plt.yticks([])
+plt.show()
 
 # Save the model checkpoint
 torch.save(model.state_dict(), 'model.ckpt')
